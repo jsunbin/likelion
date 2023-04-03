f4d50ebb4318cbf6db43837fbcf400466c438f2b
jsunbin <jsnb6n@gmail.com>
2023-04-03 22:44:03 +0900

    📝 2023.04.03 - Undefined, Null

---
+++ JavaScript/20230403/016_undefined.js
@@ -0,0 +1,10 @@
+let a;
+console.log(a);
+
+if (typeof a === "undefined") {
+  console.log("y에 아무것도 할당되지 않았습니다!")
+}
+
+if (!a) {
+  console.log("a에 아무것도 할당되지 않았습니다!")
+}
---
+++ JavaScript/20230403/017_null.js
@@ -0,0 +1,23 @@
+let value1; // undefined
+
+// 존재하지만 값이나 자료형이 존재하지 않는
+var value2 = null;
+console.log(value2); // null
+
+// let 반점수 = [10, 20, 30, null, 40, 50]
+// let 반평균 = 0;
+// for (const i of 반점수) {
+//   console.log(i)
+//   반평균 += i
+// }
+
+// 반평균
+
+// let 반점수 = [10, 20, 30, undefined, 40, 50]
+// let 반평균 = 0;
+// for (const i of 반점수) {
+//   console.log(i)
+//   반평균 += i
+// }
+
+// 반평균
