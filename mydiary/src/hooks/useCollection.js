b3ad263ae365242c5d2cac5f764970cf60b00ad5
jsunbin <jsnb6n@gmail.com>
2023-06-08 15:46:41 +0900

    ✨ Firebase - DB 데이터 불러오기

---
+++ mydiary/src/hooks/useCollection.js
@@ -0,0 +1,34 @@
+import { collection, onSnapshot } from "firebase/firestore";
+import { useEffect, useState } from "react"
+import { appFireStore } from "../firebase/config";
+
+export const useCollection = (transaction) => {
+  // transaction: 인자로 받아올 컬렉션
+
+  const [documents, setDocuments] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(collection(appFireStore, transaction),
+      (snapshot) => {
+        let result = [];
+
+        snapshot.docs.forEach((doc) => {
+          result.push({ ...doc.data(), id: doc.id });
+        })
+
+        setDocuments(result);
+        setError(null);
+      },
+      (error) => {
+        setError(error.message);
+      }
+    )
+
+    return () => {
+      unsubscribe();
+    }
+  }, []);
+
+  return { documents, error }
+} 
